58f4596d6529cda3c8a6c0ce501dce8dea5e6f90
spconger <steve_conger@msn.com>
2021-03-29 17:38:24 -0700

    second commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 <p>This application is meant to track research projects, keeping track
 of research topics, sources, and materials.</p>
 <p>First Commit: uploading structure.</p>
+<p>The second commit uploads configuration of settings.py and database, redirect of urls to app, adding of templates and static directories, base.html and index.html</p>
--- researchmanager/researchmanager/settings.py
+++ researchmanager/researchmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-3z9qn-zth)jmv-9yd#we3j@(p&3=9)qedf(4e1oqklh5hp1zt'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'research',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'researchmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER' : config('DB_USER'),
+        'PASSWORD' : config('DB_PASSWORD'),
+        'HOST' : config('DB_HOST'),
+        'PORT' : config('DB_PORT'),
     }
 }
 
